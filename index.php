2a05d0397b35eb2703cd1f8c11da05040b8d8776
Amy Mullins <amymullins@Amys-MacBook-Air-2.local>
2022-03-11 22:12:32 +0000

    added relevant text to homepage

--- index.php
+++ index.php
@@ -73,37 +73,31 @@ if(isset($_SESSION['validUser']) && !isset($_SESSION['loggedIn'])){
     </header>
     <div id="body" class="body">
     <div class="header1">
-        <h1>What is Phishing?</h1>
+        <h1 style="font-size: 28px; margin-left: 20px;">What is Phishing?</h1>
     </div>
     <div class="text">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam adipiscing vitae proin sagittis. Sagittis orci a scelerisque purus semper. Ac orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt. Elit duis tristique sollicitudin nibh sit amet commodo. In hac habitasse platea dictumst. Eu ultrices vitae auctor eu augue. Tempus egestas sed sed risus pretium quam vulputate. Est lorem ipsum dolor sit amet consectetur. Congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar. Volutpat consequat mauris nunc congue nisi vitae. Eget lorem dolor sed viverra ipsum. Eu consequat ac felis donec. A diam maecenas sed enim ut.</p>
+        <p style="font-size: 22px; margin-left: 10px;">Phishing is a type of social engineering attack that is aimed directly at you, the internet user. These attacks work by tricking the user into handing over sensitive information, such as credit card details, or login credentials. Attackers can achieve this by encouraging the user to click on a malicious link. This could be delivered to the user in an email or text message, pretending to be from a trusted source. These attacks can lead to disastrous results for the victim, such as loss of bank funds, or unauthorised purchases. In most cases, the victim of a phishing attack suffers from a financial loss.</p>
     </div>
     <div class ="header2">
-        <h2>Types Of Phishing</h2>
+        <h2 style="font-size: 28px; margin-left: 20px;">Most Common Types Of Phishing:</h2>
     </div>
     <div class="text">
-        <ul>
-            <li>Lorem ipsum dolor sit amet, </li>
-               <li>consectetur adipiscing elit,</li>
-            <li>sed do eiusmod tempor incididunt </li>
-
+        <ul style="font-size: 22px; margin-left: 10px;">
+            <li><b>Email scams</b> - the most common type of phishing attack. Attackers send emails, impersonating a trusted brand and entice the victim into clicking the malicious link.</li>
+            <br><li><b>Spear phishing</b> - similar to email scams, however the attacker does research on the company they wish to attack, so they can use real names of other individuals within the company to make the victim believe the email is from another individual within the business. </li>
+            <br><li><b>Vishing</b> - voice phishing. The attacker calls the victim's phone and urges them to take immediate action on something, which may involve handing over bank card details.</li>
+            <br><li><b>Whaling</b> - an attack where individuals of high importance, such as business CEOs are specifcally targetted in order to steal sensitive information about the business.</li>
+            <br><li><b>Smishing</b> - sending text messages to individuals, urging them to take action, which will likely involve clicking on a malicious link.</li>
         </ul>
         </div>
         <div class ="header2">
-            <h2>History of Phishing</h2>
+            <h2 style="font-size: 28px; margin-left: 20px;">History of Phishing</h2>
         </div>
         <div class="text">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam adipiscing vitae proin sagittis. Sagittis orci a scelerisque purus semper. Ac orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt. Elit duis tristique sollicitudin nibh sit amet commodo. In hac habitasse platea dictumst. Eu ultrices vitae auctor eu augue. Tempus egestas sed sed risus pretium quam vulputate. Est lorem ipsum dolor sit amet consectetur. Congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar. Volutpat consequat mauris nunc congue nisi vitae. Eget lorem dolor sed viverra ipsum. Eu consequat ac felis donec. A diam maecenas sed enim ut.</p>
+            <p style="font-size: 22px; margin-left: 10px;">The first phishing email is thought to have been sent around 1995. However, one of the first major phishing scams recorded was in May 2000, the Love Bug. This was a scam which originated in the Philippines, where internet users all over the world receieved emails with "ILOVEYOU" as the subject. The contents of the email contained an attached file and a message which said "Kindly check the attached LOVELETTER coming from me". When the victims opened the attachment, a worm was unleashed that did damage to the computer. </p>
 
         </div>
     </div>
-    <footer class="footer">
-        <nav>
-            <ul>
-                <li>Contact Us</li>
-            </ul>
-        </nav>
-    </footer>
 
 </body>
 </html>
