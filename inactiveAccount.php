af20a6cc125b40e1f0b034e5019be5e28d15d70e
Amy Mullins <amymullins@Amys-MacBook-Air-2.local>
2022-03-13 15:19:51 +0000

    database host name change

--- inactiveAccount.php
+++ inactiveAccount.php
@@ -2,7 +2,7 @@
 //this code deletes any user who has been inactive for a year. This file will be run everyday from crontabs.
 
 //define the sql connection
-$DATABASE_HOST = 'localhost';
+$DATABASE_HOST = '127.0.0.1';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
 $DATABASE_NAME = 'myProject';
